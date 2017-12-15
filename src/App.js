f4b1fa15638d4a7939a03ae70206368577559165
Renato Galvão <galvon@gmail.com>
2017-12-15 09:30:04 -0200

    Calculations done, data on table

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import base from './rebase';
 
 import './App.css';
 
-import logo from './assets/wifiw.jpg';
+import wifiwLogo from './assets/wifiw.jpg';
 
 class App extends Component {
   constructor() {
@@ -14,6 +14,7 @@ class App extends Component {
       wifiw: {
         championships: [],
         coaches: [],
+        bonus: [],
       },
       loading: true,
     }
@@ -49,10 +50,9 @@ class App extends Component {
 
   render() {
     const { championships, coaches, bonus } = this.state.wifiw;
-
-    const headerTable = championships.map(champ => <th>{champ.name}</th>);
-
+    const headerTable = championships.concat(bonus).map(champ => <th>{champ.name}</th>);
     const tableData = [];
+    const lastChampIndex = 4;
 
     coaches.forEach((coach,index) => {
       const coachResults = [];
@@ -67,22 +67,52 @@ class App extends Component {
         coachResults.push({});
       });
 
-      tableData.push({
-        name: coach,
-        total: this.getTotal(coachResults),
-        averagePerChamp: this.getAveragePerChamp(this.getTotal(coachResults), this.filterNumbers(coachResults).length),
+      bonus.forEach(item => {
+        if (item.coaches) {
+          const result = item.coaches.find(champCoach => {
+            return champCoach.name === coach;
+          });
+          if (result === undefined) return coachResults.push({});
+          return coachResults.push(result.points);
+        };
+        coachResults.push({});
       });
-    });
-    console.log(tableData);
 
+      tableData.push([
+        coach, //name
+        this.getTotal(coachResults), //total
+        this.getAveragePerChamp(this.getTotal(coachResults), this.filterNumbers(coachResults).length), //averagePerChamp
+      ].concat(coachResults));
+    });
+
+
+    tableData.sort((a, b) => {
+      if (a[1] < b[1]) return -1;
+      if (a[1] > b[1]) return 1;
+      if (a[1] == b[1] && a[lastChampIndex] < b[lastChampIndex]) return -1;
+      if (a[1] == b[1] && a[lastChampIndex] > b[lastChampIndex]) return 1;
+      return 0;
+    })
+
+    tableData.reverse();
+
+    const content = [];
+
+    if (tableData.length > 0) {
+      content.push(tableData.map(line => {
+        const lineContent = line.map(block => !isNaN(block) ? <td>{block}</td> : <td>-</td>);
+        return <tr>{lineContent}</tr>;
+      }));
+    }
+
+    console.log(content);
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="WIFIW" />
+          <img src={wifiwLogo} className="App-wifiwLogo" alt="WIFIW" />
           <h1 className="App-title">WIFIW - Corrida dos campeões 2017/2018</h1>
         </header>
-        <p className="App-intro">
           <table>
             <tr>
               <th>Posição</th>
@@ -91,11 +121,8 @@ class App extends Component {
               <th>Média</th>
               {headerTable}
             </tr>
-            <tr>
-
-            </tr>
+            {content}
           </table>
-        </p>
       </div>
     );
   }
